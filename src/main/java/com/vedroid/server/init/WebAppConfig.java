aff09470277918737ecf4b36cbfe465022f187d9
dverbovskiy <dverbovskiy@gmail.com>
2014-02-13 21:40:23 +0200

    maven filtering with profile variables, now you need to -P dv-serena-jetty profile enabled.

--- src/main/java/com/vedroid/server/init/WebAppConfig.java
+++ src/main/java/com/vedroid/server/init/WebAppConfig.java
@@ -37,6 +37,7 @@ public class WebAppConfig {
     private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
     private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
     private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
+    private static final String PROPERTY_NAME_HIBERNATE_GENERATE_DDL = "hibernate.generateDdl";
     private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
 
     @Resource
@@ -71,6 +72,7 @@ public class WebAppConfig {
         properties.put(PROPERTY_NAME_HIBERNATE_DIALECT,	env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
         properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
         properties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO));
+        properties.put(PROPERTY_NAME_HIBERNATE_GENERATE_DDL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_GENERATE_DDL));
         return properties;
     }
 
