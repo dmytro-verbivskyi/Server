81cc19788d00f107fce5b0edc56850812baa0a08
Dmitriy Verbovskiy <dverbovskiy@gmail.com>
2014-05-09 16:21:23 +0300

    first empty test

---
+++ src/test/java/com/vedroid/server/service/ShopServiceTest.java
@@ -0,0 +1,13 @@
+package com.vedroid.server.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShopServiceTest {
+
+    @Test
+    public void testFindAll() throws Exception {
+        int a = 9;
+        Assert.assertTrue(true);
+    }
+}
